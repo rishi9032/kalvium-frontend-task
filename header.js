0a66586e31455ed082f00029c39189d197fef0bf
rishi9032 <119027463+rishi9032@users.noreply.github.com>
2023-08-27 16:35:06 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header({ className, title }) {
+  return (
+    <header className={className}>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+export default Header;
